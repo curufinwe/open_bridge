b3911467a0e07143ffd128100c057838c15b76b4
coco <coco@hexgolems.com>
2015-03-22 15:50:30 +0100

    added helm overlay

--- client/helm.rb
+++ client/helm.rb
@@ -1,17 +1,21 @@
 class HelmInterface
   attr_accessor :state, :ship
 
-  def initialize(game,input)
+  def initialize(game)
     @game = game
   end
 
   def preload
+    @game.load.image("helm_nav","assets/helm_nav.png")
   end
 
   def update
     next unless ship
     @game.camera.view.x = ship.sprite.x-400
     @game.camera.view.y = ship.sprite.y-300
+    @helm_nav.x = ship.sprite.x
+    @helm_nav.y = ship.sprite.y
+    @helm_nav[:rotation] = ship.sprite[:rotation]+`Math.PI`
   end
 
   def create(input)
@@ -20,6 +24,8 @@ class HelmInterface
     @input.on("turn_right"){|mode,_| if mode == :up then @ship.set_state :throttle_rot, 0 else @ship.set_state :throttle_rot, 1 end }
     @input.on("accelerate"){|mode,_| if mode == :up then @ship.set_state :throttle_accel, 0 else @ship.set_state :throttle_accel, 1 end }
     @input.on("decelerate"){|mode,_| if mode == :up then @ship.set_state :throttle_accel, 0 else @ship.set_state :throttle_accel, -1 end }
+    @helm_nav = @game.add.sprite(0,0,"helm_nav")
+    @helm_nav.anchor.setTo(0.5,0.5)
   end
 
 end
