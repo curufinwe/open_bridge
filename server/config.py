6fa1f1f71c6b872c32d7915d604b4cdabacf03e3
Eugen Beck <eugen.beck@rwth-aachen.de>
2015-05-09 21:06:59 +0200

    More distance between ships in config

--- server/config.py
+++ server/config.py
@@ -7,9 +7,9 @@ def init_game(game):
   game.debug = True
   ship_id = game.world.addShip(t.ship_templates['debug_ship_1']())
   ship_id = game.world.addShip(t.ship_templates['debug_ship_2']())
-  game.world.ships[ship_id].x += 60
+  game.world.ships[ship_id].x += 80
   ship_id = game.world.addShip(t.ship_templates['debug_ship_3']())
-  game.world.ships[ship_id].x -= 60
+  game.world.ships[ship_id].x -= 80
   #for i in range(50):
   #  ship_id = game.world.addShip(t.ship_templates['debug_ship_1']())
   #  game.world.ships[ship_id].x += 120
