302211c56883de4f7a5fb907db9d49ba87d4662d
coco <coco@hexgolems.com>
2015-05-14 18:02:29 +0200

    merge fix

--- server/config.py
+++ server/config.py
@@ -9,14 +9,8 @@ def init_game(game):
   ship_id = game.world.addShip(t.ship_templates['debug_ship_2']())
   game.world.ships[ship_id].x += 80
   ship_id = game.world.addShip(t.ship_templates['debug_ship_3']())
-<<<<<<< HEAD
-  game.world.ships[ship_id].x -= 60
-  for i in range(50):
-    ship_id = game.world.addShip(t.ship_templates['debug_ship_1']())
-    game.world.ships[ship_id].x += 120
-=======
+
   game.world.ships[ship_id].x -= 80
   #for i in range(50):
   #  ship_id = game.world.addShip(t.ship_templates['debug_ship_1']())
   #  game.world.ships[ship_id].x += 120
->>>>>>> 2f182c187b551a80c6cb332a6d5daaf2ebf55245
